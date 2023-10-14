181fd3891be9bd3b24b4a49730b7ba5b2876fbfc
Cansy <cansyland@gmail.com>
2023-10-14 16:51:31 +0200

    update solidity version to 0.8.20

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,7 +26,7 @@ const homestead = {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.19",
+    version: "0.8.20",
     settings: {
       optimizer: {
         enabled: true,
